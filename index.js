e0c09d9b727177f61652c9ed8b0975edd5a31fb0
Andrea Cox <msandrea712@yahoo.com>
2023-04-15 09:22:49 -0400

    array map

--- index.js
+++ index.js
@@ -12,5 +12,19 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  let newTutorials = tutorials.map((str) => {
+    let modifiedStr = str.split("");
+    for (let i = 0; i < modifiedStr.length; i++) {
+      if (i === 0) {
+        modifiedStr[i] = modifiedStr[i].toUpperCase();
+      }
+      if (modifiedStr[i - 1] === " ") {
+        modifiedStr[i] = modifiedStr[i].toUpperCase();
+      }
+    }
+    modifiedStr = modifiedStr.join("");
+    return modifiedStr;
+  });
+
+  return newTutorials;
+};
